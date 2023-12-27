30457b82e0413fbd00d0a3295beb3b6b2f06588c
qizalan <92960261+qizalan@users.noreply.github.com>
2023-12-26 20:43:03 -0600

    Finish build.js script

--- build.js
+++ build.js
@@ -1,5 +1,37 @@
-const fs = require('fs');
+const { readFileSync, writeFileSync } = require('fs');
+const { globSync } = require('glob');
 
-const data = {};
+const data = {
+	// "Period #: Name": {
+	// 	"Unit #: Name": {
+	// 		"Topic #: Name": [
+	// 			[ "Q", "A" ]
+	// 		]
+	// 	}
+	// }
+};
 
-fs.writeFileSync('./questions.json', JSON.stringify(data), 'utf-8');
+const files = globSync('Period*/**/*.txt');
+
+files.forEach(f => {
+	const [ period, unit, topicTXT ] = f.split('/');
+	if (!data[period]) data[period] = {};
+	if (!data[period][unit]) data[period][unit] = {};
+	const topic = topicTXT.slice(0, topicTXT.length - '.txt'.length);
+	const file = readFileSync(f, 'utf-8');
+
+	const questions = [];
+	const groups = file.split('> ');
+	groups.forEach(group => {
+		if (!group.length) return;
+		const split = group.split('\n');
+		const lines = group.endsWith('\n\n') ? split.slice(0, split.length - 2) : split;
+		const question = lines.shift();
+		const answer = lines.join('\n');
+		questions.push([ question, answer ]);
+	});
+
+	data[period][unit][topic] = questions;
+});
+
+writeFileSync('./questions.json', JSON.stringify(data, null, '\t'), 'utf-8');
